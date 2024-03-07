64172e34662ef8fc8901bd00e2a952bd5577f541
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-03-07 20:55:41 -0300

    Commented one import of OneToMany in funcionarioEntity

--- src/main/java/app/entity/Funcionario.java
+++ src/main/java/app/entity/Funcionario.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+//import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
