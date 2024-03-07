e7982004c7a11d163f5dc72df314d4ce6196f6d0
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-03-07 15:14:49 -0300

    Add FuncionarioService and modified FuncionarioRepository

--- src/main/java/app/repository/FuncionarioRepository.java
+++ src/main/java/app/repository/FuncionarioRepository.java
@@ -1,9 +1,17 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.entity.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario,Long>{
 	
+	public List<Funcionario> findByNomeFunci(String nome);
+	 
+	public List<Funcionario> findByIdadeFunci (int idade);
+	
+	//@Query ("FROM Funcionario f WHERE M.Matricula > :Matricula")
+	public List<Funcionario> findByMatricula (String Matricula);
 }
