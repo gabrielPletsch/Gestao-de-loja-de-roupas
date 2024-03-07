aa874783ea06646a8bcdcae8996bc3e70f2a8ad8
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-03-07 20:46:09 -0300

    Add ProdutoRepository

--- src/main/java/app/repository/ProdutoRepository.java
+++ src/main/java/app/repository/ProdutoRepository.java
@@ -1,5 +1,15 @@
 package app.repository;
 
-public class ProdutoRepository {
-	
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import app.entity.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+	public List<Produto> findByNomeProd(String nome);
+
+	public List<Produto> findByValorProd(double nome);
+
+	//public List<Produto> FindByValorProd(int nome);
+
 }
