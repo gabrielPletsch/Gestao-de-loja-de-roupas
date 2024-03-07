d7e2afb375d97f0f938155d63588db5cd5762687
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-03-07 14:16:58 -0300

    Add FuncionarioRepository

--- src/main/java/app/repository/FuncionarioRepository.java
+++ src/main/java/app/repository/FuncionarioRepository.java
@@ -1,5 +1,9 @@
 package app.repository;
 
-public class FuncionarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import app.entity.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario,Long>{
+	
 }
