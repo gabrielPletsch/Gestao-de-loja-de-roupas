5672f935f052b81858db2ba03d87d05147f5e502
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-03-07 14:07:53 -0300

    Add Entity

--- src/main/java/app/entity/Funcionario.java
+++ src/main/java/app/entity/Funcionario.java
@@ -1,5 +1,38 @@
 package app.entity;
 
-public class Funcionario {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Funcionario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idFuncionario;
+	
+	@NotNull
+	private String nomeFunci;
+	
+	@NotNull
+	private int idadeFunci;
+	
+	@NotNull
+	private String matricula;
+
+	@OneToMany (mappedBy = "funcionario")
+	private List<Venda> venda;
 }
