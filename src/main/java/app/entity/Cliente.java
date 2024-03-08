fabe0ec8f4dcaaece76bb776269bc9a00db67fb0
oMaquito1000 <pedrohenriquefoletto@gmail.com>
2024-03-08 18:48:07 -0300

    ajustando erros, removi properties p n bugar

--- src/main/java/app/entity/Cliente.java
+++ src/main/java/app/entity/Cliente.java
@@ -9,11 +9,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Cliente {
 	
