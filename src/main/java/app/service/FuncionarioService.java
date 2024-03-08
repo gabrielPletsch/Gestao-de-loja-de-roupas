03bfbbfc7f8d7d3dddfa79757e6a04577d6587ee
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-03-08 18:44:37 -0300

    coment import

--- src/main/java/app/service/FuncionarioService.java
+++ src/main/java/app/service/FuncionarioService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import app.entity.Funcionario;
-import app.entity.Venda;
+//import app.entity.Venda;
 import app.repository.FuncionarioRepository;
 
 @Service
